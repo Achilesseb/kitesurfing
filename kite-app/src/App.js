e8aa3b16d52bf208461096f9bb7b779ec1305d82
Achilesseb <prisacariuvictor@gmail.com>
2022-04-03 17:59:02 +0300

    Set up map with React-leaflet && fetched spot data async

--- kite-app/src/App.js
+++ kite-app/src/App.js
@@ -1,9 +1,26 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Map from "./Components/Map/map-component";
 
 function App() {
+  const [state, setState] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(
+        "https://6246b943739ac8459191ce55.mockapi.io/spot"
+      )
+        .then((response) => response.json())
+        .then((results) => setState(results));
+    };
+    fetchData().catch((err) => console.error(err.message));
+  }, []);
+  console.log(state);
+  if (state == null) return <div>Loading</div>;
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="map">
+        <Map />
+      </div>
     </div>
   );
 }
