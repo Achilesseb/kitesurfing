032bd65c288939244a08fa803aaf79eb64b3e271
Achilesseb <prisacariuvictor@gmail.com>
2022-04-03 18:55:49 +0300

    Added loading screen on open/refresh page with Spinner

--- kite-app/src/App.js
+++ kite-app/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Map from "./Components/Map/map-component";
+import { BounceLoader } from "react-spinners";
 
 function App() {
   const [state, setState] = useState(null);
@@ -16,7 +17,13 @@ function App() {
   }, []);
   console.log(state);
 
-  if (state == null) return <div>Loading</div>;
+  if (state == null)
+    return (
+      <div className="waiting">
+        <span className="waiting-label">Loading...</span>
+        <BounceLoader color="#3b5999" size="20vh" />
+      </div>
+    );
   return (
     <div className="App">
       <div className="map">
