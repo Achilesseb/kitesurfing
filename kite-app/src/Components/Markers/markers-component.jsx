c3ad33859033fd338569b0714fa5a906a68e2f62
Achilesseb <prisacariuvictor@gmail.com>
2022-04-06 20:45:08 +0300

    Filter spots to show only those matching the conditions

--- kite-app/src/Components/Markers/markers-component.jsx
+++ kite-app/src/Components/Markers/markers-component.jsx
@@ -7,15 +7,23 @@ import "./markers-component.styles.scss";
 const SpotMarkers = () => {
   let spotData;
   const data = useSelector((data) => data);
-  console.log(data);
-  if (data.filters.length === 0) spotData = data.spots;
-  else
-    spotData = data.spots.filter(
-      (spots) =>
-        spots.country === data.filters[0] &&
-        spots.probability == data.filters[1]
+  const spots = data.spots;
+  const filters = data.filters;
+  if (filters.length === 0) spotData = spots;
+  if (filters[0] === null) {
+    spotData = spots.filter((spots) => spots.probability == filters[1]);
+  }
+  if (filters[1] === null) {
+    spotData = spots.filter((spots) =>
+      spots.country.includes(
+        String(filters[0].slice(0, 1)).toUpperCase() + filters[0].slice(1)
+      )
     );
-  console.log(spotData);
+  }
+  if (filters[0] && filters[1])
+    spotData = spots.filter(
+      (spots) => spots.country === filters[0] && spots.probability == filters[1]
+    );
   return spotData.map((spot) => (
     <Marker position={[spot.lat, spot.long]} key={spot.id}>
       <Popup>
